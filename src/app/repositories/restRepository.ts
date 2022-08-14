7550f85772cd5fabbe73d8c742e3186c07f07b3c
Ramon Trepat <r.trepis@gmail.com>
2022-08-14 12:23:50 +0200

    Added repostiroy with fuction getAll and your test

---
+++ src/app/repositories/restRepository.ts
@@ -0,0 +1,14 @@
+interface RestRepositoryType<T> {
+  getAll: () => Promise<T[]>;
+}
+
+class RestRepsitory<T> implements RestRepositoryType<T> {
+  constructor(private url: string) {}
+
+  async getAll() {
+    const response = await fetch(this.url);
+    return response.json();
+  }
+}
+
+export default RestRepsitory;
