2aa0536dc42e029d8d31bf416e306b8dd94c69ba
David Lozano <dawgun@hotmail.com>
2022-08-14 12:19:56 +0200

    Created case in robotsReducer for loadRobots

--- src/features/robots/reducer/actionsCreator/actionsCreator.ts
+++ src/features/robots/reducer/actionsCreator/actionsCreator.ts
@@ -2,6 +2,4 @@ import { createAction } from "@reduxjs/toolkit";
 import Robot from "../../models/Robot";
 import { actionTypes } from "../actionTypes/actionTypes";
 
-export const loadRobotsActionCreator = createAction<Robot[]>(
-  actionTypes.loadRobots
-);
+export const loadRobots = createAction<Robot[]>(actionTypes.loadRobots);
