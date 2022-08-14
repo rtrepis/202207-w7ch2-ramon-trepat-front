71a0004293702a6d01f990342cabe5e46746f269
David Lozano <dawgun@hotmail.com>
2022-08-14 22:28:54 +0200

    Deleted mock dispatch test

--- src/components/RobotCardList/RobotCardList.test.tsx
+++ src/components/RobotCardList/RobotCardList.test.tsx
@@ -1,14 +1,7 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../app/store";
 import RobotCardList from "./RobotCardList";
-
-const mockDispatch = jest.fn();
-
-jest.mock("react-redux", () => ({
-  ...jest.requireActual("react-redux"),
-  useDispatch: () => mockDispatch,
-}));
 
 describe("Given a RobotCardList component", () => {
   describe("When it's instantiated", () => {
@@ -35,16 +28,6 @@ describe("Given a RobotCardList component", () => {
       const listofRobots = screen.getAllByRole("article");
 
       expect(listofRobots).toHaveLength(initialRobotList.length);
-    });
-
-    test("Then should call dispatch hook", async () => {
-      render(
-        <Provider store={store}>
-          <RobotCardList />{" "}
-        </Provider>
-      );
-
-      await waitFor(() => expect(mockDispatch).toHaveBeenCalled());
     });
   });
 });
