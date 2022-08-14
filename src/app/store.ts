81ec9669f853b72667ab8603ee52a788720e9b1d
David Lozano <dawgun@hotmail.com>
2022-08-14 12:36:47 +0200

    Added robotReducer to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { robotReducer } from "../features/robots/reducer/robotReducer";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    robots: robotReducer,
   },
 });
 
