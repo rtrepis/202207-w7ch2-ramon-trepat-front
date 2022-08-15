ea20ae824dfa710a1cfaf01cd3272da6ea65aef9
Ramon Trepat <r.trepis@gmail.com>
2022-08-15 23:46:57 +0200

    HotFix style shadow Button

--- src/components/Button/ButtonStyled.ts
+++ src/components/Button/ButtonStyled.ts
@@ -6,8 +6,7 @@ const ButtonStyled = styled.button`
   color: #ffffff;
   font-size: 0.8rem;
   font-weight: 700;
-  padding: 15px;
-  box-shadow: 0 1px 5px 0 #000000;
+  padding: 10px;
   border: solid #337fed 1px;
   text-decoration: none;
   display: inline-block;
