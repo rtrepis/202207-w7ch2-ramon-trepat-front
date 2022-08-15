3646e960cdbc22a17fe5ce333e48a2f826aa6a2e
Ramon Trepat <r.trepis@gmail.com>
2022-08-15 23:49:39 +0200

    Added title page and css

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import RobotCardList from "./components/RobotCardList/RobotCardList";
 
 const App = () => {
   return (
-    <div className="main-container">
-      <RobotCardList />
-    </div>
+    <>
+      <div className="headers">
+        <h1>Robots</h1>
+      </div>
+      <div className="main-container">
+        <RobotCardList />
+      </div>
+    </>
   );
 };
 
